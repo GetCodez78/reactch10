5aa98e722b0ba802fbbd49f3f227ed33d9a77397
GitCodez78 <ziheng.li@gmail.com>
2017-08-28 13:32:34 -0400

    add firebase settiongs.

--- src/App.js
+++ src/App.js
@@ -19,12 +19,15 @@ class App extends Component {
     this.getBooks();  // 1. update state onload 
   }
 
+  /* eslint-disable */
+  
   getBooks() {   // updates state 
     axios.request({
       method:'get',
       url:'https://www.googleapis.com/books/v1/volumes?q='+this.state.text
     }).then((response) => {
       this.setState({books:response.data.items}, () => {
+        
         console.log(this.state);
       });
   }).catch((error) => {
